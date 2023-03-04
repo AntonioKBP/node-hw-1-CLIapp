95b1b06d5ffb40f19ee5d4a75cfc0398e7982e8c
Anton Kovsh <kovsh_anton@ukr.net>
2023-03-04 17:44:47 +0200

    upd

--- contacts.js
+++ contacts.js
@@ -2,6 +2,52 @@ const path = require("path");
 
 const fs = require("fs").promises;
 
-const contactPath = path.join("db", "contacts.json");
+const contactsPath = path.join("db", "contacts.json");
 
-console.log(contactPath);
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  //   ...твой код
+}
+
+const readFile = async () => {
+  try {
+    const readContactsJson = await fs.readFile(contactsPath);
+
+    const json = JSON.parse(readContactsJson);
+    console.log(json);
+
+    return json;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+readFile();
+
+const writeFile = async (value) => {
+  const readContactsJson = await fs.writeFile(
+    contactsPath,
+    JSON.stringify(value)
+  );
+
+  const json = JSON.parse(readContactsJson);
+
+  return json;
+};
+
+module.exports = {
+  readFile,
+  writeFile,
+};
