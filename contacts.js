6a98701bd3545a60138608f509c64f90eb4c1ecb
Anton Kovsh <kovsh_anton@ukr.net>
2023-03-04 14:14:12 +0200

    upd

---
+++ contacts.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const fs = require("fs").promises;
+
+const contactPath = path.join("db", "contacts.json");
+
+console.log(contactPath);
---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello Node!!!");
+const path = require("path");
+
+const fs = require("fs").promises;
+
+/**
+ * Read and write parameters.
+ * @returns {Promise<void>}
+ */
+
+const readFile = async () => {
+  try {
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+console.log(path);
